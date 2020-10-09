888c525e07cdf663afd2d0b4ce7c9c0177096ea7
Anisberto dos Reis do Nascimento <anisbertoos@gmail.com>
2020-10-09 00:33:26 -0300

    Update form-doador.component.ts

--- front/frontApi/src/app/pageDoador/form-doador/form-doador.component.ts
+++ front/frontApi/src/app/pageDoador/form-doador/form-doador.component.ts
@@ -24,6 +24,4 @@ export class FormDoadorComponent implements OnInit {
       this.router.navigate(['/cadastro-usuario'], { queryParams: { id: '1' }, queryParamsHandling: 'merge' });
     }
   }
-
 }
-[{ "id": 1, "nome": "Beronice Alves dos Reis", "telefone": "62 9 92947507", "email": "Beronice@gmail.com" }, { "id": 2, "nome": "Anisberto dos Reis do Nascimento", "telefone": "62 9 92947507", "email": "Anisberto@gmail.com" }, { "id": 3, "nome": "Aline dos Reis do Nascimento", "telefone": "62 9 92947507", "email": "Aline@gmail.com" }]
