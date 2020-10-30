886be785e1b55b55df7288234e062aa870fa55a5
Ricardo Alves Roberto <contato@rdrtech.com.br>
2020-10-30 20:48:37 -0300

    deploy back no heroku

--- back2/doaju/src/main/java/br/com/doaju/security/config/JwtCustomClaimsTokenEnhancer.java
+++ back2/doaju/src/main/java/br/com/doaju/security/config/JwtCustomClaimsTokenEnhancer.java
@@ -1,6 +1,7 @@
 package br.com.doaju.security.config;
 
 import java.util.HashMap;
+import java.util.Map;
 
 import org.springframework.security.oauth2.common.DefaultOAuth2AccessToken;
 import org.springframework.security.oauth2.common.OAuth2AccessToken;
@@ -12,13 +13,13 @@ public class JwtCustomClaimsTokenEnhancer implements TokenEnhancer {
 	@Override
 	public OAuth2AccessToken enhance(OAuth2AccessToken accessToken, OAuth2Authentication authentication) {
 		if (authentication.getPrincipal() instanceof AuthUser) {
-			var authUser = (AuthUser) authentication.getPrincipal();
+			AuthUser authUser = (AuthUser) authentication.getPrincipal();
 			
-			var info = new HashMap<String, Object>();
+			Map info = new HashMap<String, Object>();
 			info.put("nome_usuario", authUser.getNomeUser());
 			info.put("usuario_id", authUser.getUserId());
 			
-			var oAuth2AccessToken = (DefaultOAuth2AccessToken) accessToken;
+			DefaultOAuth2AccessToken oAuth2AccessToken = (DefaultOAuth2AccessToken) accessToken;
 			oAuth2AccessToken.setAdditionalInformation(info);
 		}
 		
